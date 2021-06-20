af7b0da57ce171ca3a49df995213401819d3f2af
bulir <aboandrus@gamil.com>
2021-06-20 22:27:21 +0200

    fix server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/front'));
+app.use(express.static(__dirname + '/dist/Front'));
 
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/front/'})
+  res.sendFile('index.html', {root: 'dist/Front/'})
 );
 
 // Start the app by listening on the default
