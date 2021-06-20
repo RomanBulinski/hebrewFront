ef3d8140f1cf0984520ca9debd4be237ece8543f
bulir <aboandrus@gamil.com>
2021-06-20 20:38:33 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/fronthebrew'));
+
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/fronthebrew/'})
+);
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
