0bec55b8eb73c97ea27800aebb723b53e03de4da
bulir <aboandrus@gamil.com>
2021-06-21 21:30:43 +0200

    next

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen("fronthebrew.herokuapp.com"  || 3000);
