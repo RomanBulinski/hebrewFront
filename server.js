73a147d9858a61a9b04da3e89df5a7d3bb8e6d40
bulir <aboandrus@gamil.com>
2021-06-21 19:59:41 +0200

    front: update server.js 2

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/Front'));
+app.use(express.static(__dirname + '/dist/front'));
 
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/Front/'})
+  res.sendFile('index.html', {root: 'dist/front/'})
 );
 
 // Start the app by listening on the default
 // Heroku port
 // app.listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 8080,
+app.listen(process.env.posrt || 3000,
   () =>
   {
     console.log("Express server listening on port %d in %s mode",
