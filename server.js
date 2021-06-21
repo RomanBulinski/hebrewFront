0af54b9f3d6137b731ef4b7579127807f36d007c
bulir <aboandrus@gamil.com>
2021-06-21 19:48:14 +0200

    front: update server.js 2

--- server.js
+++ server.js
@@ -15,4 +15,9 @@ app.get('/*', (req, res) =>
 // Heroku port
 // app.listen(process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 8080, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); });
+app.listen(process.env.PORT || 8080,
+  () =>
+  {
+    console.log("Express server listening on port %d in %s mode",
+    this.address().port, app.settings.env);
+  });
